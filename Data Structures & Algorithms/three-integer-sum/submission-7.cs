b26342e9481fc9385a9a6573bb3325efb8a10fa6
chayki <47690874+chayki@users.noreply.github.com>
2026-07-23 16:04:55 +0530

    Add: three-integer-sum - submission-7

---
+++ Data Structures & Algorithms/three-integer-sum/submission-7.cs
@@ -0,0 +1,43 @@
+//Sorting + HashSet based approach
+// Time complexity - same as that of two pointer approach O(n^2)
+// space complexity - O(n)
+public class Solution {
+    public List<List<int>> ThreeSum(int[] nums) {
+        List<List<int>> result = new List<List<int>>();
+        Dictionary<int,int> map = new ();
+        foreach (int num in nums)
+        {
+            if (!map.ContainsKey(num))
+            {
+                map[num]=0;
+            }
+            map[num]++;
+        }
+
+        Array.Sort(nums);
+
+        for (int i = 0; i < nums.Length-1; ++i)
+        {
+            map[nums[i]]--;
+            if (i > 0 && nums[i] == nums[i-1]) continue;
+            for (int j = i+1; j < nums.Length; ++j)
+            {
+                map[nums[j]]--;
+                if (j > i+1 && nums[j] == nums[j-1]) continue;
+                int target = -(nums[i]+nums[j]);
+
+                if (map.ContainsKey(target) && map[target] > 0)
+                {
+                    result.Add(new List<int> {nums[i], nums[j], target});
+                }
+            }
+
+            for (int j = i+1; j < nums.Length; ++j)
+            {
+                map[nums[j]]++;
+            }
+        }
+
+        return result;       
+    }
+}
