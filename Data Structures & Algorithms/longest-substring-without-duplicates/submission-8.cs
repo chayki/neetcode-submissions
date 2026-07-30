02a4ec19f7d9529414594b977a2a6e5e20383ff9
chayki <47690874+chayki@users.noreply.github.com>
2026-07-30 13:32:08 +0530

    Add: longest-substring-without-duplicates - submission-8

---
+++ Data Structures & Algorithms/longest-substring-without-duplicates/submission-8.cs
@@ -0,0 +1,27 @@
+// Can we optimize further? yes.
+// In solution 3 - when a duplicate found we are moving the anchor point one step at a time,
+// we can optimize this by storing last index of every char found till then and move anchor directly to left+1
+// Asymptotic time complexity remains same at O(n) - each index is visited at the worst case
+// space complexity - O(m) - number of uniq chars
+public class Solution {
+    public int LengthOfLongestSubstring(string s) {
+        if (s.Length <= 1) return s.Length;
+        Dictionary<char,int> charIndexMap = new Dictionary<char,int>(s.Length);
+        int anchor = 0, end = 1;
+        int maxLength = 1;
+        charIndexMap[s[0]] = 0;
+        while (end < s.Length)
+        {
+            if (charIndexMap.ContainsKey(s[end]) && charIndexMap[s[end]] >= anchor)
+            {
+                anchor = charIndexMap[s[end]]+1;
+            }
+            charIndexMap[s[end]] = end;
+            int length = end-anchor+1;
+            maxLength = Math.Max(length, maxLength);
+            ++end;
+        }
+
+        return maxLength;
+    }
+}
