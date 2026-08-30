53a815bff1948987c1b5df71407eddf4086983df
chayki <47690874+chayki@users.noreply.github.com>
2026-08-30 12:40:39 +0530

    Add: search-for-word-ii - submission-8

---
+++ Data Structures & Algorithms/search-for-word-ii/submission-8.cs
@@ -0,0 +1,90 @@
+public class Solution {
+    private PrefixTree trie = new PrefixTree();
+    private int[] dr = new int[] {0,0,1,-1};
+    private int[] dc = new int[] {1,-1,0,0};
+
+    public List<string> FindWords(char[][] board, string[] words) {
+        var result = new List<string>();
+        foreach (string word in words)
+        {
+            trie.InsertWord(word);
+        }
+
+        int m = board.Length;
+        int n = board[0].Length;
+        for (int i = 0; i < m; ++i)
+        {
+            for (int j = 0; j < n; ++j)
+            {
+                FindWordsDFS(board, this.trie.Root, i, j, result);
+            }
+        }
+
+        return result.ToList();    
+    }
+
+    public void FindWordsDFS(char[][] board, Node node, int i, int j, List<string> result)
+    {
+        // Is this state valid?
+        if (i < 0 || i >= board.Length || j < 0 || j >= board[0].Length || board[i][j] == '#') return;
+        if (!node.Children.ContainsKey(board[i][j])) return;
+
+        // choose
+        node = node.Children[board[i][j]];
+        var cellState = board[i][j];
+        board[i][j] = '#';
+
+        // Is this a solution?
+        if (node.IsEndOfWord)
+        {
+            result.Add(node.Word);
+            node.IsEndOfWord = false;
+        }
+
+        // iterate through each neighbour and recurse
+        for (int d = 0; d < 4; ++d)
+        {
+            int nr = i+dr[d];
+            int nc = j+dc[d];
+            FindWordsDFS(board, node, nr, nc, result);
+        }
+
+        // restore state
+        board[i][j] = cellState;
+    }
+}
+
+public class PrefixTree
+{
+    public Node Root {get; private set;}
+    
+    public PrefixTree()
+    {
+        this.Root = new Node();
+    }
+
+    public void InsertWord(string word)
+    {
+        var currNode = this.Root;
+        for(int i = 0; i < word.Length; ++i)
+        {
+            if (!currNode.Children.ContainsKey(word[i])) currNode.Children.Add(word[i], new Node());
+            var childNode = currNode.Children[word[i]];
+            currNode = childNode;
+        }
+        currNode.Word = word;
+        currNode.IsEndOfWord = true;
+    }
+}
+
+public class Node
+{
+    public bool IsEndOfWord {get; set;}
+    public string Word {get; set;}
+    public Dictionary<char, Node> Children {get; private set;}
+
+    public Node()
+    {
+        this.Children = new Dictionary<char,Node>();
+    }
+}
