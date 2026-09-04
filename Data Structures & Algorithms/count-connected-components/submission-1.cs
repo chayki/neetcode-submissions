e2e1103676a06aab84e7f0fdd54c2c169d8cc0b4
chayki <47690874+chayki@users.noreply.github.com>
2026-09-04 11:55:30 +0530

    Add: count-connected-components - submission-1

---
+++ Data Structures & Algorithms/count-connected-components/submission-1.cs
@@ -0,0 +1,73 @@
+public class Solution {
+    public int CountComponents(int n, int[][] edges) {
+        UnionFind uf = new UnionFind(n);
+        int totalComponents = n;
+        foreach(var edge in edges)
+        {
+            if (!uf.AreConnected(edge[0], edge[1]))
+            {
+                uf.Union(edge[0], edge[1]);
+                totalComponents--;
+            }
+        }
+
+        return totalComponents;
+    }
+}
+
+public class UnionFind
+{
+    private int[] parent;
+    private int[] size;
+
+    public UnionFind(int n)
+    {
+        parent = new int[n];
+        size = new int[n];
+
+        for (int i = 0; i < n; ++i)
+        {
+            parent[i] = i;
+            size[i] = 1;
+        }
+    }
+
+    public bool AreConnected(int x, int y)
+    {
+        int xRoot = FindRoot(x);
+        int yRoot = FindRoot(y);
+
+        return xRoot == yRoot;
+    }
+
+    public int FindRoot(int x)
+    {
+        if (parent[x] == x) return x;
+        parent[x] = FindRoot(parent[x]);
+        return parent[x];
+    }
+
+    public void Union(int x, int y)
+    {
+        int xRoot = FindRoot(x);
+        int yRoot = FindRoot(y);
+
+        int xSize = size[xRoot];
+        int ySize = size[yRoot];
+
+        if (xSize > ySize)
+        {
+            parent[yRoot] = xRoot;
+            size[xRoot] += size[yRoot];
+        }
+        else
+        {
+            parent[xRoot] = yRoot;
+            size[yRoot] += size[xRoot];
+        }
+    }
+}
+
+/* I will use union-find approach for bringing the time complexity further down.
+Total number of connected components before processing any edge would be n.
+For each edge being processed, if it results in an union operation I would decrement the component count by 1 */
